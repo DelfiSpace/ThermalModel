feedf2394473124fabb16e757d2f399a523a38db
ceciler100 <48725868+ceciler100@users.noreply.github.com>
2020-06-09 17:50:33 +0200

    Fixing an error

--- tests/test_SolarFlux.m
+++ tests/test_SolarFlux.m
@@ -6,8 +6,8 @@ function test_suite=test_SolarFlux
     initTestSuite;
 
 function test_SolarFlux_scalar
-    assertTrue(SolarFlux_perso(3) > 1413 && SolarFlux_perso(3) < 1415) %3 january: perigee and value of Solar flux is 1414 W/m^2
+    assertTrue(round(SolarFlux_perso(3)) == 1414) %3 january: perigee and value of Solar flux is 1414 W/m^2
     %assertTrue(true)
     
 function test_SolarFlux_vector
-    assertEqual(round(SolarFlux_perso([3 184])),[1414 1322]); %day 184 : apogee, Solar flux= 1322 W/m^2
+    assertEqual(round(SolarFlux_perso([3 184])), [1414 1322]); %day 184 : apogee, Solar flux= 1322 W/m^2
