bf5244777bed386316adf21f6714638857b04310
ceciler100 <cecile.ruiz@ensta-paris.fr>
2020-06-29 17:17:34 +0200

    Simplify the use of addThermalConnection
    Replace the link array in the inputs by the numbers of the nodes to connect

--- Reference code/addThermalConnection.m
+++ Reference code/addThermalConnection.m
@@ -1,10 +1,12 @@
-function out = addThermalConnection(in, link, value)
+function out = addThermalConnection(in, link1, link2, value)
+
+% link1, link2: number of the nodes connected 
 
 out = zeros(size(in, 1) + 1, size(in, 2) + 1);
 out(1:size(in, 1), 1 : size(in, 2)) = in;
 
-% connection X+ Y+
-a = [1 0 -1 0 0 0];
-out(end, 1 : length(link)) = link;
-out(1 : length(link), end) = -link';
-out(end, end) = value;
+out(end, link1) = 1 ;
+out(end, link2) = -1 ;
+out(link1, end) = -1 ;
+out(link2, end) = 1 ;
+out(end, end) = value ;
